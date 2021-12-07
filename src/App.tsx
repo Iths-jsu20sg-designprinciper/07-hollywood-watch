17ce0b402355840213f9c0fca849c641ee265d32
David Andersson <david.andersson@zocom.se>
2021-12-07 09:38:42 +0100

    Static mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="component">
+			<header>
+				<h1> Hollywood watch </h1>
+				<input type="text" />
+				<span className="magnify-search">🔍</span>
+			</header>
+
+			<main className="card-grid">
+				<section className="card ">
+					<div className="image"> image </div>
+					<h3> My neighbour Totoro </h3>
+					<p> 2021-09-28 </p>
+				</section>
+				
+				<section className="card">
+					<div className="image"> image </div>
+					<h3> My neighbour Totoro </h3>
+					<p> 2021-09-28 </p>
+				</section>
+				
+				<section className="card">
+					<div className="image"> image </div>
+					<h3> My neighbour Totoro </h3>
+					<p> 2021-09-28 </p>
+				</section>
+				
+				<section className="card">
+					<div className="image"> image </div>
+					<h3> My neighbour Totoro </h3>
+					<p> 2021-09-28 </p>
+				</section>
+				
+				<section className="card">
+					<div className="image"> image </div>
+					<h3> My neighbour Totoro </h3>
+					<p> 2021-09-28 </p>
+				</section>
+				
+				<section className="card">
+					<div className="image"> image </div>
+					<h3> My neighbour Totoro </h3>
+					<p> 2021-09-28 </p>
+				</section>
+				
+			</main>
+		</div>
+	);
 }
 
 export default App;
